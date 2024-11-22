cb6853ee03edc51669471e208094aa823577554d
dishant <aryadishant414@gmail.com>
2024-11-22 11:29:09 +0530

    modifications

--- Personal-Portfolio/src/components/contact/Contact.jsx
+++ Personal-Portfolio/src/components/contact/Contact.jsx
@@ -16,8 +16,8 @@ export default function Contact() {
       <div className="right">
         <h2>Contact.</h2>
         <form onSubmit={handleSubmit}>
-          <input type="text" placeholder="Email" />
-          <textarea placeholder="Message"></textarea>
+          <input type="text" placeholder="Email" required/>
+          <textarea placeholder="Message" required></textarea>
           <button type="submit">Send</button>
           {message && <span>Thanks, I'll reply ASAP :)</span>}
         </form>
