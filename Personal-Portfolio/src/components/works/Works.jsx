d5f954beea25125fcf04049804ad29c739658b60
dishant <aryadishant414@gmail.com>
2024-11-22 08:31:40 +0530

    modifications

--- Personal-Portfolio/src/components/works/Works.jsx
+++ Personal-Portfolio/src/components/works/Works.jsx
@@ -3,29 +3,30 @@ import "./works.scss";
 
 export default function Works() {
   const [currentSlide, setCurrentSlide] = useState(0);
+
   const data = [
-    {
-      id: "1",
-      icon: "./assets/mobile.png",
-      title: "Shopping Website",
-      desc:
-        "Developed a full-featured e-commerce website with functionalities similar to Flipkart, enabling seamless shopping, product browsing, and secure transactions.",
-      img:
-        "https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/10/attachment_100040756-e1538485934255.jpeg?auto=format&q=60&fit=max&w=930",
-      gitLink: 'https://github.com/aryadishant414/MERN_FLIPKART_2024',
-      liveDemo: '/',
-    },
-    {
-      id: "2",
-      icon: "./assets/globe.png",
-      title: "Movie Application",
-      desc:
-        "Built a dynamic movie app using React, featuring movie browsing, search, and detailed information for an enhanced user experience.",
-      img:
-        "https://i.pinimg.com/originals/e9/c9/2f/e9c92f7869d682a6fa5a97fb8a298f30.jpg",
-      gitLink: 'https://github.com/aryadishant414/movieApp2024',
-      liveDemo: '/',
-    },
+    // {
+    //   id: "1",
+    //   icon: "./assets/mobile.png",
+    //   title: "Shopping Website",
+    //   desc:
+    //     "Developed a full-featured e-commerce website with functionalities similar to Flipkart, enabling seamless shopping, product browsing, and secure transactions.",
+    //   img:
+    //     "https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/10/attachment_100040756-e1538485934255.jpeg?auto=format&q=60&fit=max&w=930",
+    //   gitLink: 'https://github.com/aryadishant414/MERN_FLIPKART_2024',
+    //   // liveDemo: '/',
+    // },
+    // {
+    //   id: "2",
+    //   icon: "./assets/globe.png",
+    //   title: "Movie Application",
+    //   desc:
+    //     "Built a dynamic movie app using React, featuring movie browsing, search, and detailed information for an enhanced user experience.",
+    //   img:
+    //     "https://i.pinimg.com/originals/e9/c9/2f/e9c92f7869d682a6fa5a97fb8a298f30.jpg",
+    //   gitLink: 'https://github.com/aryadishant414/movieApp2024',
+    //   // liveDemo: '/',
+    // },
     // {
     //   id: "3",
     //   icon: "./assets/writing.png",
@@ -35,11 +36,111 @@ export default function Works() {
     //   img:
     //     "https://i.pinimg.com/originals/a9/f6/94/a9f69465d972a004ad581f245d6ad581.jpg",
     // },
+
+
+
+    {
+      id: "1",
+      icon: "./assets/mobile.png",
+      title: "Shopping Website",
+      desc:
+        "Developed a full-featured e-commerce website with functionalities similar to Flipkart, enabling seamless shopping, product browsing, and secure transactions.",
+      img:
+        "https://cdn.dribbble.com/userupload/8627406/file/original-00075081fc348a864a534261c8078ab0.png?resize=1200x900&vertical=center",
+      gitLink: 'https://github.com/aryadishant414/MERN_FLIPKART_2024',
+      // liveDemo: '/',
+    },
+    {
+      id: "2",
+      icon: "./assets/globe.png",
+      title: "Movie Application",
+      desc:
+        "Built a dynamic movie app using React, featuring movie browsing, search, and detailed information for an enhanced user experience.",
+      img:
+        "https://cdn.dribbble.com/userupload/16314060/file/original-5145dc0a3df723c0cb40707ff2f07d8e.png?resize=1200x900&vertical=center",
+      gitLink: 'https://github.com/aryadishant414/movieApp2024',
+      // liveDemo: '/',
+    },
+    {
+      id: "3",
+      icon: "./assets/globe.png",
+      title: "Trading Leaderboard",
+      desc:
+        "An interactive trading system leaderboard showcasing the top 10 leaders based on their trading performance and rankings.",
+      img:
+        "https://cdn.dribbble.com/userupload/15666775/file/original-9fd6e78dea6dbfae98955bbed30f4493.png?resize=1200x901&vertical=center",
+      gitLink: 'https://github.com/aryadishant414/TradingSystemLeaderboard2024',
+      // liveDemo: '/',
+    },
+    {
+      id: "4",
+      icon: "./assets/globe.png",
+      title: "Blog Website",
+      desc:
+        "A dynamic blog website featuring engaging articles, seamless navigation, and personalized content for readers.",
+      img:
+        "https://cdn.dribbble.com/userupload/5300978/file/original-63652306e44124f3352690b835c86e55.jpg?resize=1200x900&vertical=center",
+      gitLink: 'https://github.com/aryadishant414/megaBlogWebsite',
+      // liveDemo: '/',
+    },
+    {
+      id: "5",
+      icon: "./assets/globe.png",
+      title: "Brand Page",
+      desc:
+        "A sleek single-page brand website showcasing the essence of the brand through captivating visuals and concise storytelling.",
+      img:
+        "https://cdn.dribbble.com/userupload/16488421/file/original-6d6be5aed9e7303620a1144f904b31ff.jpg?resize=1200x900&vertical=center",
+      gitLink: 'https://github.com/aryadishant414/brand-page',
+      // liveDemo: '/',
+    },
+    {
+      id: "6",
+      icon: "./assets/globe.png",
+      title: "Dice Game",
+      desc:
+        "A fun and interactive dice game where players test their luck and strategy with each roll.",
+      img:
+        "https://cdn.dribbble.com/userupload/14396095/file/original-5e45ae03ce9065af6851e64b3fa8fdf4.jpg?resize=1200x900&vertical=center",
+      gitLink: 'https://github.com/aryadishant414/Dice-Game',
+      // liveDemo: '/',
+    },
+    {
+      id: "7",
+      icon: "./assets/globe.png",
+      title: "Foody Zone",
+      desc:
+        "An intuitive food discovery website, offering a wide range of restaurants, dishes, and seamless online ordering experiences.",
+      img:
+        "https://cdn.dribbble.com/users/508588/screenshots/4942666/media/ac35cd08bc5dc1d3c0b73671711bab26.jpg?resize=800x600&vertical=center",
+      gitLink: 'https://github.com/aryadishant414/Foody-Zone',
+      // liveDemo: '/',
+    },
+    {
+      id: "8",
+      icon: "./assets/globe.png",
+      title: "To Do List",
+      desc:
+        "A simple and efficient to-do list app that allows users to create, read, update, and delete tasks for better task management.",
+      img:
+        "https://cdn.dribbble.com/userupload/14124616/file/original-2f1d19559f30d37af42774ed286b7450.png?resize=1200x900&vertical=center",
+      gitLink: 'https://github.com/aryadishant414/Firebase-CRUD',
+      // liveDemo: '/',
+    },
+
+
+
+
+
+
+
+
+
   ];
 
   const handleClick = (way) => {
     way === "left"
-      ? setCurrentSlide(currentSlide > 0 ? currentSlide - 1 : 1)
+      ? setCurrentSlide(currentSlide > 0 ? currentSlide - 1 : 7)
       : setCurrentSlide(currentSlide < data.length - 1 ? currentSlide + 1 : 0);
   };
   
@@ -60,14 +161,14 @@ export default function Works() {
                   <h2>{d.title}</h2>
                   <p>{d.desc}</p>
                   <a href={`${d.gitLink}`}><span>See Code on Githhub</span></a>
-                  <a href={`${d.liveDemo}`}><span>View Live Demo</span></a>
+                  {/* <a href={`${d.liveDemo}`}><span>View Live Demo</span></a> */}
                 </div>
               </div>
               <div className="right">
-                <img
-                  src="https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/10/attachment_100040756-e1538485934255.jpeg?auto=format&q=60&fit=max&w=930"
-                  alt=""
-                />
+              <img
+                src={d.img}
+                alt={d.title} // Optional: Alt text
+              />
               </div>
             </div>
           </div>
--- Personal-Portfolio/src/data.js
+++ Personal-Portfolio/src/data.js
@@ -1,40 +1,93 @@
 export const featuredPortfolio = [
+    // {
+    //   id: 1,
+    //   title: "Social Media App",
+    //   img:
+    //     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+    // },
+    // {
+    //   id: 2,
+    //   title: "Rampa UI Design",
+    //   img:
+    //     "https://cdn.dribbble.com/users/702789/screenshots/15054318/media/4ea5d492b7b07eebc9528ff960794879.png?compress=1&resize=1200x900",
+    // },
+    // {
+    //   id: 3,
+    //   title: "E-commerce Web Design",
+    //   img:
+    //     "https://cdn.dribbble.com/users/1387827/screenshots/15466426/media/deb2dca6762cd3610321c98bfccb0b72.png?compress=1&resize=1200x900",
+    // },
+    // {
+    //   id: 4,
+    //   title: "Relax Mobile App",
+    //   img:
+    //     "https://cdn.dribbble.com/users/4095861/screenshots/15467417/media/d6a15c416626f12b31fa5ca1db192572.png?compress=1&resize=1200x900",
+    // },
+    // {
+    //   id: 5,
+    //   title: "Hero Web Design",
+    //   img:
+    //     "https://cdn.dribbble.com/users/5031392/screenshots/15467520/media/c36b3b15b25b1e190d081abdbbf947cf.png?compress=1&resize=1200x900",
+    // },
+    // {
+    //   id: 6,
+    //   title: "Banking App",
+    //   img:
+    //     "https://cdn.dribbble.com/users/3307260/screenshots/15468444/media/447b4501f7a145b05e11c70199a9c417.jpg?compress=1&resize=1200x900",
+    // },
+
+
     {
       id: 1,
-      title: "Social Media App",
+      title: "Shopping Website",
       img:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+        "https://cdn.dribbble.com/userupload/9643919/file/original-964e7c7ed9c77cfa90cf565645804613.png?resize=1024x768&vertical=center",
     },
     {
       id: 2,
-      title: "Rampa UI Design",
+      title: "Movie Application",
       img:
-        "https://cdn.dribbble.com/users/702789/screenshots/15054318/media/4ea5d492b7b07eebc9528ff960794879.png?compress=1&resize=1200x900",
+        "https://cdn.dribbble.com/userupload/6187551/file/original-f3c21312ab67ac6875e605464dc4de49.png?resize=1024x768&vertical=center",
     },
     {
       id: 3,
-      title: "E-commerce Web Design",
+      title: "Trading Leaderboard",
       img:
-        "https://cdn.dribbble.com/users/1387827/screenshots/15466426/media/deb2dca6762cd3610321c98bfccb0b72.png?compress=1&resize=1200x900",
+        "https://cdn.dribbble.com/userupload/14709572/file/original-500c548d38ef5a1a956fef42b5fa37f5.png?resize=1024x768&vertical=center",
     },
     {
       id: 4,
-      title: "Relax Mobile App",
+      title: "Blog Website",
       img:
-        "https://cdn.dribbble.com/users/4095861/screenshots/15467417/media/d6a15c416626f12b31fa5ca1db192572.png?compress=1&resize=1200x900",
+        "https://cdn.dribbble.com/userupload/6518782/file/original-f29f5dd8c7b1f1977e6fc1f966ecedb3.png?resize=1024x768&vertical=center",
     },
     {
       id: 5,
-      title: "Hero Web Design",
+      title: "Brand Page",
       img:
-        "https://cdn.dribbble.com/users/5031392/screenshots/15467520/media/c36b3b15b25b1e190d081abdbbf947cf.png?compress=1&resize=1200x900",
+        "https://cdn.dribbble.com/userupload/7003738/file/original-dfb383775be14d9767f44326d9bd0311.png?resize=1024x768&vertical=center",
     },
     {
       id: 6,
-      title: "Banking App",
+      title: "Dice Game",
       img:
-        "https://cdn.dribbble.com/users/3307260/screenshots/15468444/media/447b4501f7a145b05e11c70199a9c417.jpg?compress=1&resize=1200x900",
+        "https://cdn.dribbble.com/userupload/15140095/file/original-7c6484acc97ec41d28a9e3df34ce39aa.jpg?resize=1024x1500&vertical=center",
     },
+    {
+      id: 7,
+      title: "Foody Zone",
+      img:
+        "https://cdn.dribbble.com/users/6175319/screenshots/15856926/media/f8bbd8e466de3af9ea252bbed81c8e8f.jpg?resize=1000x750&vertical=center",
+    },
+    {
+      id: 7,
+      title: "To do List",
+      img:
+        "https://cdn.dribbble.com/userupload/4160697/file/original-7535daedab6754c4b3d5f36b09c2133e.png?resize=1024x768&vertical=center",
+    },
+
+
+
   ];
   
   export const webPortfolio = [
